3eabea69a441065f0573a8ee2f847d3f348f87a1
Jimmy Ko <3071329+jimmyko@users.noreply.github.com>
2020-02-27 12:49:31 +0000

    Base project for selenium.

---
+++ src/main/java/co/uk/independent/test/App.java
@@ -0,0 +1,18 @@
+package co.uk.independent.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        WebDriver driver = new FirefoxDriver();
+    }
+}
